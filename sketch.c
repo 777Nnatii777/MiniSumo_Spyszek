b20549d35a5b4f5a994bb86ffcb50764e2dda19c
Natan Młynarczyk <150261644+777Nnatii777@users.noreply.github.com>
2024-11-13 20:26:20 +0100

    File_For_testing
    boolean zamienić na int jak nie działa

--- sketch.c
+++ sketch.c
@@ -1,6 +1,6 @@
 //sketch.c*
 //Created on: Nov 03, 2024
-//Author: Natan, Kamil, Kacper*/
+//Author: Natan, Kamil*/
 
 #include "main.h"
 #include "motor.h"
@@ -12,11 +12,12 @@ void setup(void) {
 }
 extern motor_t hmot;
 
-extern simpleSensor_t sensorRR;
-extern simpleSensor_t sensorLL;
 extern simpleSensor_t sensorL;
 extern simpleSensor_t sensorC;
 extern simpleSensor_t sensorR;
+
+
+
 
 
 extern volatile uint16_t adcReadout[5];
@@ -25,23 +26,26 @@ extern volatile uint16_t adcReadout[5];
 
 void startLoop(void) {
 
+	boolean lewyczujnik = simpleSensorGetState(&sensorL);
+	boolean prawyczujnik = simpleSensorGetState(&sensorR);
+	boolean srodkowyczujnik = simpleSensorGetState(&sensorC);
 
-    uint16_t sensorValueLeft = adcReadout[0];
-    uint16_t sensorValueRight = adcReadout[1];
+  uint16_t sensorValueLeft = adcReadout[0];
+  uint16_t sensorValueRight = adcReadout[1];
 
 
-    if ((sensorValueLeft < 220) || (sensorValueRight) < 220) {
+   /* if ((sensorValueLeft < 3300) || (sensorValueRight) < 3300) {
 
 
         motorSetDirection(&hmot, MOTOR_BACKWARD);
         motorSetPWM(&hmot, 200);
-        return;
+return;
 
     }
+*/
 
-
-
-    else if (simpleSensorGetState(&sensorC)) {
+/*
+    if (simpleSensorGetState(&sensorC)) {
 
         motorSetDirection(&hmot, MOTOR_FORWARD);
         motorSetPWM(&hmot, 255);
@@ -61,4 +65,5 @@ void startLoop(void) {
         motorSetDirection(&hmot, MOTOR_RIGHT);
         motorSetPWM(&hmot, 150);
     }
+    */
 }
